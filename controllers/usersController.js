2e2313768fea10fd0befad776a28602c51676358
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-08-04 22:51:07 -0300

    Section user done!

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,15 @@
 const users = require('../Data/user.json');
-console.log(users)
+// console.log(users)
 module.exports = {
     index: (req, res) => {
-        return res.render('users',{
+        return res.render('users', {
             users
         });
+    },
+    profile: (req, res) => {
+        const id = +req.params.id;
+        console.log(id)
+        const user = users.find(user => user.id === +req.params.id);
+        return res.render('profile', { user })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersController = require('../controllers/usersController')
 /* GET users listing. */
 router.get('/', usersController.index);
+router.get('/profile/:id', usersController.profile);
 
 module.exports = router;
